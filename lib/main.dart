66e92ee3310f865228bdfad8e013677df4c5452a
a7med <a7med.assem@gmail.com>
2020-01-28 12:36:30 +0200

    Working with Providers & Listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shop/providers/products_provider.dart';
 import 'package:shop/screens/product_detail_screen.dart';
 import 'package:shop/screens/product_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "MyShop",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
+    return ChangeNotifierProvider(
+      builder: (context) =>ProductProvider(),
+      child: MaterialApp(
+        title: "MyShop",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'
+        ),
+        home: ProductOverView(),
+        routes: {
+          ProductDetail.routeName : (context) => ProductDetail(),
+        },
       ),
-      home: ProductOverView(),
-      routes: {
-        ProductDetail.routeName : (context) => ProductDetail(),
-      },
     );
   }
 }
