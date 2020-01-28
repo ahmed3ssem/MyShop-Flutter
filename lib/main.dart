97908fcf6fe9330754c6d2fba0cae420895d5378
a7med <a7med.assem@gmail.com>
2020-01-28 10:07:06 +0200

    Defining a Data Model

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "MyShop",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
---
+++ lib/widgets/product_overview_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProductOverView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
