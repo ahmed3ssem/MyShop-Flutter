457fd6cf18cbae76c484379c459038acb99141ba
a7med <a7med.assem@gmail.com>
2020-01-28 18:06:30 +0200

    Adding Product Detail Data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -12,6 +12,25 @@ class ProductDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text(productData.title),
       ),
+      body:SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(productData.imageUrl,fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 10,),
+            Text('\$${productData.price}',style: TextStyle(color: Colors.grey,fontSize: 20),),
+            SizedBox(height: 10,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(productData.description,textAlign: TextAlign.center,softWrap: true,),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
